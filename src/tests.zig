dbbebde82298c57e4252540d50d91de1e013632e
Simon Symeonidis <lethaljellybean@gmail.com>
2023-02-11 18:06:36 -0500

    Get rid of relative paths in imports

--- src/tests.zig
+++ src/tests.zig
@@ -1,8 +1,8 @@
 test {
-    _ = @import("./ds/test/dynarr_test.zig");
-    _ = @import("./ds/test/list_test.zig");
-    _ = @import("./ds/test/stack_test.zig");
-    _ = @import("./ds/test/bintree_test.zig");
-    _ = @import("./ds/test/ring_test.zig");
-    _ = @import("./ds/test/tree_test.zig");
+    _ = @import("ds/test/dynarr_test.zig");
+    _ = @import("ds/test/list_test.zig");
+    _ = @import("ds/test/stack_test.zig");
+    _ = @import("ds/test/bintree_test.zig");
+    _ = @import("ds/test/ring_test.zig");
+    _ = @import("ds/test/tree_test.zig");
 }
